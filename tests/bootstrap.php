347ffe1b707b0cccae8ebb9c3d89dee8149b41a1
Darren Ethier <darren@roughsmootheng.in>
2017-04-22 17:19:48 -0400

    update test bootstrap/config for ticketing add-on [touch: 10623]

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,11 +6,15 @@
 use EETests\bootstrap\AddonLoader;
 
 $core_tests_dir = dirname(dirname(dirname(__FILE__))) . '/event-espresso-core/tests/';
+//if still don't have $core_tests_dir, then let's check tmp folder.
+if (! is_dir($core_tests_dir)) {
+    $core_tests_dir = '/tmp/event-espresso-core/tests/';
+}
 require $core_tests_dir . 'includes/CoreLoader.php';
 require $core_tests_dir . 'includes/AddonLoader.php';
 
 define('EE_TICKETING_PLUGIN_DIR', dirname(dirname(__FILE__)) . '/');
-define('EE_TICKETING_TESTS_DIR', EE_TICKETING_PLUGIN_DIR . 'tests');
+define('EE_TICKETING_TESTS_DIR', EE_TICKETING_PLUGIN_DIR . 'tests/');
 
 
 $addon_loader = new AddonLoader(
